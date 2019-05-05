3bd8f6a1aea48bc93435e952f9d1ce7716032d96
proserge <34370028+proserge@users.noreply.github.com>
2019-05-05 14:59:09 +0300

    linters check

--- app/controllers/youtube_search_controller.rb
+++ app/controllers/youtube_search_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class YoutubeSearchController < ApplicationController
   def show
     @show_results = SearchExecutor.new.call
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root 'youtube_search#show' 
+  root 'youtube_search#show'
   get 'youtube_search/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
